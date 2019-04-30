eb157017f1f79602492cf1f25f7ffd73e14f581e
Ephraime Godinez <exgodinez@innodata.com>
2019-04-30 19:06:15 +0800

    initial commit

---
+++ Clustering/Dot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clustering
+{
+    public class Dot
+    {
+        static int count = 1;
+        public int id { get; }
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public Dot(int x, int y)
+        {
+            this.id = count++;
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return ($"P({this.x}, {this.y})");
+        }
+    }
+}
---
+++ Clustering/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Clustering
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSpawn = new System.Windows.Forms.Button();
+            this.pnlMap = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // btnSpawn
+            // 
+            this.btnSpawn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSpawn.ForeColor = System.Drawing.Color.Lime;
+            this.btnSpawn.Location = new System.Drawing.Point(1, 443);
+            this.btnSpawn.Name = "btnSpawn";
+            this.btnSpawn.Size = new System.Drawing.Size(75, 23);
+            this.btnSpawn.TabIndex = 1;
+            this.btnSpawn.Text = "Spawn";
+            this.btnSpawn.UseVisualStyleBackColor = true;
+            this.btnSpawn.Click += new System.EventHandler(this.btnSpawn_Click);
+            // 
+            // pnlMap
+            // 
+            this.pnlMap.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pnlMap.Location = new System.Drawing.Point(1, 2);
+            this.pnlMap.Name = "pnlMap";
+            this.pnlMap.Size = new System.Drawing.Size(522, 435);
+            this.pnlMap.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(524, 522);
+            this.Controls.Add(this.btnSpawn);
+            this.Controls.Add(this.pnlMap);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Clustering";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSpawn;
+        private System.Windows.Forms.Panel pnlMap;
+    }
+}
+
